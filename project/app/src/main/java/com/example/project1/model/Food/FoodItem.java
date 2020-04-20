01c591a69a7e1573f29f0777b80394d29ecf958b
Tala Jamal (Silwadi) <63348645+talaSilwadi@users.noreply.github.com>
2020-04-20 19:06:34 +0300

    food part

--- project/app/src/main/java/com/example/project1/model/Food/FoodItem.java
+++ project/app/src/main/java/com/example/project1/model/Food/FoodItem.java
@@ -3,4 +3,35 @@ package com.example.project1.model.Food;
 import com.example.project1.model.abstractData.AbstractItem;
 
 public class FoodItem extends AbstractItem {
+
+    String chefName ;
+    String foodType ;
+
+    public FoodItem(){
+        super();
+    }
+
+    public FoodItem(String itemName, String description, int imageID, int price, String chefName, String foodType) {
+        super(itemName, description, imageID, price);
+        this.chefName = chefName;
+        this.foodType = foodType;
+    }
+
+    public String getChefName() {
+        return chefName;
+    }
+
+    public void setChefName(String chefName) {
+        this.chefName = chefName;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+
 }
